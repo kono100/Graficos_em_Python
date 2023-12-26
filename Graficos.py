48e69ef08788deb2747a92eeddd0e74dc9af5d34
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-12-26 20:48:45 -0300

    Update Graficos.py

--- Graficos.py
+++ Graficos.py
@@ -756,3 +756,54 @@ ani = animation.FuncAnimation(fig, scope.update, emitter, interval=50,
                               blit=True, save_count=100)
 
 plt.show()
+
+
+
+
+
+
+
+
+
+
+# ================================================================
+
+
+
+
+
+
+
+
+
+# Grafico 18
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+rng = np.random.default_rng(19680801)
+
+# example data
+mu = 106  # mean of distribution
+sigma = 17  # standard deviation of distribution
+x = rng.normal(loc=mu, scale=sigma, size=420)
+
+num_bins = 42
+
+fig, ax = plt.subplots()
+
+# the histogram of the data
+n, bins, patches = ax.hist(x, num_bins, density=True)
+
+# add a 'best fit' line
+y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
+     np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
+ax.plot(bins, y, '--')
+ax.set_xlabel('Value')
+ax.set_ylabel('Probability density')
+ax.set_title('Histogram of normal distribution sample: '
+             fr'$\mu={mu:.0f}$, $\sigma={sigma:.0f}$')
+
+# Tweak spacing to prevent clipping of ylabel
+fig.tight_layout()
+plt.show()
