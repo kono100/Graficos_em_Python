c077f74c9498c15d62d39043b8e03f3fcf92ccc5
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-12-26 21:05:18 -0300

    Update Graficos.py

--- Graficos.py
+++ Graficos.py
@@ -912,3 +912,83 @@ t = np.arange(0., 5., 0.2)
 # red dashes, blue squares and green triangles
 plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
 plt.show()
+
+
+
+
+
+
+
+
+
+
+# ================================================================
+
+
+
+
+
+
+
+
+
+# Grafico 21
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib.patches as patches
+import matplotlib.path as path
+
+fig, axs = plt.subplots(2)
+
+np.random.seed(19680801)  # Fixing random state for reproducibility
+
+# histogram our data with numpy
+data = np.random.randn(1000)
+n, bins = np.histogram(data, 50)
+
+# get the corners of the rectangles for the histogram
+left = bins[:-1]
+right = bins[1:]
+bottom = np.zeros(len(left))
+top = bottom + n
+
+# we need a (numrects x numsides x 2) numpy array for the path helper
+# function to build a compound path
+XY = np.array([[left, left, right, right], [bottom, top, top, bottom]]).T
+
+# get the Path object
+barpath = path.Path.make_compound_path_from_polys(XY)
+
+# make a patch out of it, don't add a margin at y=0
+patch = patches.PathPatch(barpath)
+patch.sticky_edges.y[:] = [0]
+axs[0].add_patch(patch)
+axs[0].autoscale_view()
+
+nrects = len(left)
+nverts = nrects*(1+3+1)
+verts = np.zeros((nverts, 2))
+codes = np.ones(nverts, int) * path.Path.LINETO
+codes[0::5] = path.Path.MOVETO
+codes[4::5] = path.Path.CLOSEPOLY
+verts[0::5, 0] = left
+verts[0::5, 1] = bottom
+verts[1::5, 0] = left
+verts[1::5, 1] = top
+verts[2::5, 0] = right
+verts[2::5, 1] = top
+verts[3::5, 0] = right
+verts[3::5, 1] = bottom
+
+barpath = path.Path(verts, codes)
+
+# make a patch out of it, don't add a margin at y=0
+patch = patches.PathPatch(barpath)
+patch.sticky_edges.y[:] = [0]
+axs[1].add_patch(patch)
+axs[1].autoscale_view()
+
+plt.show()
+
