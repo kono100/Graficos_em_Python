5fe0187e7f68fe8ff3f806d6e6671ae6af99309d
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-12-26 21:04:48 -0300

    Update Graficos.py

--- Graficos.py
+++ Graficos.py
@@ -882,3 +882,33 @@ ani = animation.FuncAnimation(fig, prepare_animation(bar_container), 50,
                               repeat=False, blit=True)
 plt.show()
 
+
+
+
+
+
+
+
+
+
+# ================================================================
+
+
+
+
+
+
+
+
+
+# Grafico 20
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+
+# red dashes, blue squares and green triangles
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+plt.show()
